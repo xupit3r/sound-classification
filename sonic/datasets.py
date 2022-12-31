a24f24a23720697a57a67e6cb7caa3c9ea975971
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-31 02:31:45 -0500

    added in some basic dataset config reading

--- sonic/datasets.py
+++ sonic/datasets.py
@@ -1,2 +1,24 @@
+import csv as csv
+
+DATASETS_DIR = '.datasets'
+
+def read_dataset_config(name='', file_key='', class_key=''):
+  with open(f'{DATASETS_DIR}/{name}/config.csv') as file:
+    reader = csv.DictReader(file, delimiter=',')
+
+    config = []
+    for row in reader:
+      config.append([row[file_key], row[class_key]])
+
+    return config
+
 def get_urban_sounds():
+  config = read_dataset_config(
+    name='urban_sounds', 
+    file_key='slice_file_name',
+    class_key='classID'
+  )
+
+  print(config)
+
   return ()
---
+++ sound.py
@@ -0,0 +1,3 @@
+from sonic.datasets import get_urban_sounds
+
+get_urban_sounds()
