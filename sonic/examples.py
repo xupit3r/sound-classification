a1cadd398595120800ffb2989b2ee76dc5fa2876
Joe D'Alessandro <joe@thejoeshow.net>
2023-01-29 07:23:53 -0500

    provide functionality to show a spectrogram  * include both linear and logarithmic

---
+++ sonic/examples.py
@@ -0,0 +1,68 @@
+from sonic.features import extract_features, extract_directories
+from sonic.interesting import interesting_segments
+from sonic.preprocess import denoise, signal_frames
+from sonic.review import display_classes, spectrogram
+from sonic.utils import get_sound_dirs
+import matplotlib.pyplot as plt
+import soundfile as sf
+
+SOUND_FILES = ".test_data"
+
+
+def show_interesting_segments(SOUND_FILES):
+    sound_file = f"{SOUND_FILES}/city/traffic/city-traffic-outdoor.wav"
+
+    vector = extract_features(sound_file=sound_file)
+
+    print(vector.shape)
+
+    TEST_DIRECTORIES = get_sound_dirs(f"{SOUND_FILES}/city")
+
+    class_vectors, class_names = extract_directories(dirs=TEST_DIRECTORIES)
+
+    display_classes(
+        class_vectors, class_names, "spectral_centroid_mean", "energy_entropy_mean"
+    )
+
+    interesting, blocks, signal = interesting_segments(
+        f"{SOUND_FILES}/city/ambience/street-ambience.wav", base_block=1, base_step=0.05
+    )
+
+    fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True)
+
+    # plot the original signal
+    ax[0].set_title("original")
+    ax[0].plot(signal)
+
+    # plot the blocks view of the interesting segments
+    ax[1].set_title("blocks")
+    ax[1].plot(blocks)
+
+    # plot the intersting signal (combined blocks)
+    ax[2].set_title("interesting")
+    ax[2].plot(interesting)
+
+    plt.show()
+
+
+def show_denoising():
+    signal, sample_rate = sf.read(f"{SOUND_FILES}/city/ambience/street-ambience.wav")
+
+    denoised = denoise(signal)
+
+    fig, axs = plt.subplots(2)
+    axs[0].set_title("original signal")
+    axs[0].plot(signal)
+    axs[1].set_title("denoised signal")
+    axs[1].plot(denoised)
+    plt.show()
+
+    # # prepare frames from the denoised signal
+    frames = signal_frames(denoised, sample_rate)
+
+    plt.plot(frames)
+    plt.show()
+
+
+def show_spectrogram():
+    spectrogram(f"{SOUND_FILES}/anomaly/joe.wav")
--- sonic/review.py
+++ sonic/review.py
@@ -1,3 +1,5 @@
+import librosa
+import librosa.display
 import matplotlib.pyplot as plt
 import soundfile as sf
 import numpy as np
@@ -39,4 +41,28 @@ def display_classes(class_vectors, class_names, x_label="", y_label=""):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend()
+    plt.show()
+
+
+def spectrogram(sound_file, n_fft=2048, hop_length=512):
+    y, sr = librosa.load(sound_file)
+    D = librosa.amplitude_to_db(
+        np.abs(librosa.stft(y, hop_length=hop_length)), ref=np.max
+    )
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+
+    # figure 1
+    img = librosa.display.specshow(D, y_axis="linear", x_axis="time", sr=sr, ax=ax[0])
+    ax[0].set(title="Linear-frequency power spectrogram")
+    ax[0].label_outer()
+
+    # figure 2
+    librosa.display.specshow(
+        D, y_axis="log", sr=sr, hop_length=hop_length, x_axis="time", ax=ax[1]
+    )
+    ax[1].set(title="Log-frequency power spectrogram")
+    ax[1].label_outer()
+
+    fig.colorbar(img, ax=ax, format="%+2.f dB")
     plt.show()
