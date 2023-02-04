1e03df86ec670fdd6c4928447512599e01f3a2a4
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-04 09:47:32 -0500

    build and run our CNN model

--- example.py
+++ example.py
@@ -155,10 +155,10 @@ train_spectrogram_ds = make_spec_ds(train_ds)
 val_spectrogram_ds = make_spec_ds(val_ds)
 test_spectrogram_ds = make_spec_ds(test_ds)
 
-if display_ds_spectrograms:
-    for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
-        break
+for example_spectrograms, example_spect_labels in train_spectrogram_ds.take(1):
+    break
 
+if display_ds_spectrograms:
     rows = 3
     cols = 3
     n = rows * cols
@@ -172,3 +172,57 @@ if display_ds_spectrograms:
         ax.set_title(label_names[example_spect_labels[i].numpy()])
 
     plt.show()
+
+# setup datasets to cache and prefetch to help with performance
+train_spectrogram_ds = (
+    train_spectrogram_ds.cache().shuffle(10000).prefetch(tf.data.AUTOTUNE)
+)
+val_spectrogram_ds = val_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+test_spectrogram_ds = test_spectrogram_ds.cache().prefetch(tf.data.AUTOTUNE)
+
+# we will build a CNN to do this work since the spectrograms are 2D
+input_shape = example_spectrograms.shape[1:]
+print("Input shape:", input_shape)
+num_labels = len(label_names)
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+norm_layer = layers.Normalization()
+
+# Fit the state of the layer to the spectrograms with `Normalization.adapt`.
+norm_layer.adapt(data=train_spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+model = models.Sequential(
+    [
+        layers.Input(shape=input_shape),
+        # Downsample the input.
+        layers.Resizing(32, 32),
+        # Normalize.
+        norm_layer,
+        # push through the CNN
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.Conv2D(64, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.25),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dropout(0.5),
+        layers.Dense(num_labels),
+    ]
+)
+
+model.summary()
+
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+EPOCHS = 10
+history = model.fit(
+    train_spectrogram_ds,
+    validation_data=val_spectrogram_ds,
+    epochs=EPOCHS,
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
