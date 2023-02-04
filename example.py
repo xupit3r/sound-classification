51fd2c1cb311030dc6d906883ddfd26adcc4bb92
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-04 09:20:06 -0500

    spectrogram fun!

--- example.py
+++ example.py
@@ -6,6 +6,10 @@ import seaborn as sns
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+display_waveforms = False
+display_spectrogram = False
+
+
 # good idea to set random seed to be able to reproduce
 seed = 42
 tf.random.set_seed(seed)
@@ -62,23 +66,77 @@ for example_audio, example_labels in train_ds.take(1):
 
 
 # plat some waves for a few classes!
-label_names[[1, 1, 3, 0]]
+if display_waveforms:
+    label_names[[1, 1, 3, 0]]
 
-rows = 3
-cols = 3
-n = rows * cols
-fig, axes = plt.subplots(rows, cols, figsize=(16, 9))
+    rows = 3
+    cols = 3
+    n = rows * cols
+    fig, axes = plt.subplots(rows, cols, figsize=(16, 9))
 
-for i in range(n):
-    if i >= n:
-        break
-    r = i // cols
-    c = i % cols
-    ax = axes[r][c]
-    ax.plot(example_audio[i].numpy())
-    ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
-    label = label_names[example_labels[i]]
-    ax.set_title(label)
-    ax.set_ylim([-1.1, 1.1])
+    for i in range(n):
+        if i >= n:
+            break
+        r = i // cols
+        c = i % cols
+        ax = axes[r][c]
+        ax.plot(example_audio[i].numpy())
+        ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
+        label = label_names[example_labels[i]]
+        ax.set_title(label)
+        ax.set_ylim([-1.1, 1.1])
 
-plt.show()
+    plt.show()
+
+
+def get_spectrogram(waveform):
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
+
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+
+    return spectrogram
+
+
+def plot_spectrogram(spectrogram, ax):
+    if len(spectrogram.shape) > 2:
+        assert len(spectrogram.shape) == 3
+        spectrogram = np.squeeze(spectrogram, axis=-1)
+    # Convert the frequencies to log scale and transpose, so that the time is
+    # represented on the x-axis (columns).
+    # Add an epsilon to avoid taking a log of zero.
+    log_spec = np.log(spectrogram.T + np.finfo(float).eps)
+    height = log_spec.shape[0]
+    width = log_spec.shape[1]
+    X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+    Y = range(height)
+    ax.pcolormesh(X, Y, log_spec)
+
+
+if display_spectrogram:
+    for i in range(3):
+        label = label_names[example_labels[i]]
+        waveform = example_audio[i]
+        spectrogram = get_spectrogram(waveform)
+
+        print("Label:", label)
+        print("Waveform shape:", waveform.shape)
+        print("Spectrogram shape:", spectrogram.shape)
+        print("Audio playback")
+
+        fig, axes = plt.subplots(2, figsize=(12, 8))
+        timescale = np.arange(waveform.shape[0])
+        axes[0].plot(timescale, waveform.numpy())
+        axes[0].set_title("Waveform")
+        axes[0].set_xlim([0, 16000])
+
+        plot_spectrogram(spectrogram.numpy(), axes[1])
+        axes[1].set_title("Spectrogram")
+        plt.suptitle(label.title())
+        plt.show()
