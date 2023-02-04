0af7e94f13f05ca6d8b8249fe4d2509e287c9e8d
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-04 07:41:10 -0500

    unfucked my fuckups with the dataset download  * i now understand how those parameters work 😃

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ seed = 42
 tf.random.set_seed(seed)
 np.random.seed(seed)
 
-DATASET_PATH = ".datasets/mini_speech_commands"
+DATASET_PATH = ".cached_datasets/tensorflow/mini_speech_commands"
 data_dir = pathlib.Path(DATASET_PATH)
 
 if not data_dir.exists():
@@ -20,5 +20,9 @@ if not data_dir.exists():
         origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
         extract=True,
         cache_dir=".cached_datasets",
-        cache_subdir="mini_speech_commands",
+        cache_subdir="tensorflow",
     )
+
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[(commands != "README.md") & (commands != ".DS_Store")]
+print("Commands:", commands)
