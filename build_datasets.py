c04f9e468f6e381f0d7fa86e5ea8f06379fdf508
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-04 06:52:33 -0500

    default dataset feature to mfcc

--- build_datasets.py
+++ build_datasets.py
@@ -2,4 +2,4 @@ from sonic.datasets import build_urban_sounds
 from sonic.features import mfcc, centroid, bandwidth, melspectrogram, contrast
 
 print("building urban sounds dataset...")
-build_urban_sounds(feature=melspectrogram)
+build_urban_sounds(feature=mfcc)
