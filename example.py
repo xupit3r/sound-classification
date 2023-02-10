6f95e46851c12820873fd7edfeed37a492a44dfe
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-10 05:14:53 -0500

    moved and parameterized the model export

--- example.py
+++ example.py
@@ -7,6 +7,7 @@ from sonic.datasets import (
     make_spec_ds,
 )
 from sonic.review import get_spectrogram
+from sonic.models import save_model, load_model
 import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -167,49 +168,9 @@ plt.bar(x_labels, tf.nn.softmax(prediction[0]))
 plt.title("No")
 plt.show()
 
-# now build a class that can be used to export a model and make running
-# it later much easier
-class ExportModel(tf.Module):
-    def __init__(self, model):
-        self.model = model
-
-        # Accept either a string-filename or a batch of waveforms.
-        # YOu could add additional signatures for a single wave, or a ragged-batch.
-        self.__call__.get_concrete_function(x=tf.TensorSpec(shape=(), dtype=tf.string))
-        self.__call__.get_concrete_function(
-            x=tf.TensorSpec(shape=[None, 16000], dtype=tf.float32)
-        )
-
-    @tf.function
-    def __call__(self, x):
-        # If they pass a string, load the file and decode it.
-        if x.dtype == tf.string:
-            x = tf.io.read_file(x)
-            x, _ = tf.audio.decode_wav(
-                x,
-                desired_channels=1,
-                desired_samples=16000,
-            )
-            x = tf.squeeze(x, axis=-1)
-            x = x[tf.newaxis, :]
-
-        x = get_spectrogram(x)
-        result = self.model(x, training=False)
-
-        class_ids = tf.argmax(result, axis=-1)
-        class_names = tf.gather(label_names, class_ids)
-        return {
-            "predictions": result,
-            "class_ids": class_ids,
-            "class_names": class_names,
-        }
-
-
-# test the model export
-export = ExportModel(model)
-print(export(tf.constant(str(data_dir / "no/01bb6a2a_nohash_0.wav"))))
-
 # save the model and reload it
-tf.saved_model.save(export, str(model_dir / "saved"))
-imported = tf.saved_model.load(str(model_dir / "saved"))
+save_model(model, get_spectrogram, label_names, "example_model")
+imported = load_model("example_model")
+
+# test the loaded model
 print(imported(waveform[tf.newaxis, :]))
--- sonic/models.py
+++ sonic/models.py
@@ -1,5 +1,49 @@
 from tensorflow import keras
-from tensorflow.keras import layers, regularizers
+from tensorflow.keras import layers
+import tensorflow as tf
+import pathlib
+
+MODEL_PATH = ".models"
+model_dir = pathlib.Path(MODEL_PATH)
+
+# now build a class that can be used to export a model and make running
+# it later much easier
+class ExportModel(tf.Module):
+    def __init__(self, model, representation, label_names):
+        self.model = model
+        self.representation = representation
+        self.label_names = label_names
+
+        # Accept either a string-filename or a batch of waveforms.
+        # YOu could add additional signatures for a single wave, or a ragged-batch.
+        self.__call__.get_concrete_function(x=tf.TensorSpec(shape=(), dtype=tf.string))
+        self.__call__.get_concrete_function(
+            x=tf.TensorSpec(shape=[None, 16000], dtype=tf.float32)
+        )
+
+    @tf.function
+    def __call__(self, x):
+        # If they pass a string, load the file and decode it.
+        if x.dtype == tf.string:
+            x = tf.io.read_file(x)
+            x, _ = tf.audio.decode_wav(
+                x,
+                desired_channels=1,
+                desired_samples=16000,
+            )
+            x = tf.squeeze(x, axis=-1)
+            x = x[tf.newaxis, :]
+
+        x = self.representation(x)
+        result = self.model(x, training=False)
+
+        class_ids = tf.argmax(result, axis=-1)
+        class_names = tf.gather(self.label_names, class_ids)
+        return {
+            "predictions": result,
+            "class_ids": class_ids,
+            "class_names": class_names,
+        }
 
 
 def sound_model_1(height=173, width=40, channels=1, num_classes=10):
@@ -63,3 +107,14 @@ def sound_model_2(height=173, width=40, channels=1, num_classes=10):
     )
 
     return model, "sound_model_2"
+
+
+def save_model(model, representation, label_names, model_name="saved"):
+    export = ExportModel(model, representation, label_names)
+    tf.saved_model.save(export, str(model_dir / model_name))
+    return export
+
+
+def load_model(model_name="saved"):
+    imported = tf.saved_model.load(str(model_dir / model_name))
+    return imported
