0a5b39851b016b92be2f9471915719de52d80781
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-15 04:03:57 -0500

    pitch shifted the cat name audio.... 🤷‍♀️

--- fix_audio.py
+++ fix_audio.py
@@ -1,10 +1,14 @@
 import os
 import soundfile as sf
 from librosa.util import fix_length
+from librosa.effects import pitch_shift
+from random import randrange
 
-for subdir, dirs, files in os.walk("sonic/datasets/cat_names"):
+for subdir, dirs, files in os.walk("sonic/custom_datasets/cat_names"):
     for file in files:
         if file.endswith(".wav"):
             full_path = os.path.join(subdir, file)
             signal, sample_rate = sf.read(full_path)
-            sf.write(full_path, fix_length(signal, size=10624), sample_rate)
+            step = randrange(-5, 5)
+            shifted = pitch_shift(signal, sample_rate, n_steps=step)
+            sf.write(full_path, fix_length(shifted, size=10624), sample_rate)
