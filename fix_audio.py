90c81d7fa42764e77f8066f491655b6d06b75540
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-15 04:07:15 -0500

    added some random noise....

--- fix_audio.py
+++ fix_audio.py
@@ -1,5 +1,6 @@
 import os
 import soundfile as sf
+import numpy as np
 from librosa.util import fix_length
 from librosa.effects import pitch_shift
 from random import randrange
@@ -10,5 +11,6 @@ for subdir, dirs, files in os.walk("sonic/custom_datasets/cat_names"):
             full_path = os.path.join(subdir, file)
             signal, sample_rate = sf.read(full_path)
             step = randrange(-5, 5)
-            shifted = pitch_shift(signal, sample_rate, n_steps=step)
+            shifted = pitch_shift(signal, sr=sample_rate, n_steps=step)
+            noisey = shifted + 0.009 * np.random.normal(0, 1, len(shifted))
             sf.write(full_path, fix_length(shifted, size=10624), sample_rate)
