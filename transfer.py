a623204e276dba0ef2d15e49a19dadf475f26351
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-15 06:53:53 -0500

    working through a transfer learning example

---
+++ transfer.py
@@ -0,0 +1,36 @@
+import os
+
+from librosa import resample
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import soundfile as sf
+
+yamnet_model_handle = "https://tfhub.dev/google/yamnet/1"
+yamnet_model = hub.load(yamnet_model_handle)
+
+testing_wav_file_name = tf.keras.utils.get_file(
+    "miaow_16k.wav",
+    "https://storage.googleapis.com/audioset/miaow_16k.wav",
+    cache_dir="./",
+    cache_subdir=".test_data",
+)
+
+# Utility functions for loading audio files and making sure the sample rate is correct.
+
+
+@tf.function
+def load_wav_16k_mono(filename):
+    """Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio."""
+    signal, sr = sf.read(filename)
+    signal = resample(signal, orig_sr=sr, target_sr=16000)
+    return signal
+
+
+testing_wav_data = load_wav_16k_mono(testing_wav_file_name)
+
+plt.plot(testing_wav_data)
+plt.show()
